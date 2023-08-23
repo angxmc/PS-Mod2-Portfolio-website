e25c46ed4067829aa929233fffa62b615cd9fe82
angxmc <2017xchen@gmail.com>
2023-08-23 14:43:44 -0400

    uppercase App

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app";
-
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
