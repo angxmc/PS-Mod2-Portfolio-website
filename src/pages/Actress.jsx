c098b73649ba010e168fa62fb39839a279626883
Angelina Chen <angexmc@seas.upenn.edu>
2026-05-04 15:33:35 -0700

    removed video player

--- src/pages/Actress.jsx
+++ src/pages/Actress.jsx
@@ -8,7 +8,6 @@ export default function Actress() {
       director: "Fernando Frias",
       year: 2017,
       role: "Lin",
-      video: ``,
       link: "",
     },
     {
@@ -16,7 +15,6 @@ export default function Actress() {
       director: "Shuyao Chen",
       year: 2018,
       role: "The Daughter",
-      video: "",
       link: "",
     },
     {
@@ -24,7 +22,6 @@ export default function Actress() {
       director: "Shuyao Chen",
       year: 2021,
       role: "Daughter",
-      video: ``,
       link: "",
     },
   ];
@@ -36,19 +33,15 @@ export default function Actress() {
 
       <div className="videoSec">
         {projects.map((project, index) => (
-          <div key={index} className="act-section-left">
-            <video controls>
-              <source src={project.video} type="video/mp4" />
-            </video>
-
-            <div className="act-section-right">
-              <div className="projectDetails">
-                <h3 className="projectTitle">{project.title}</h3>
-                <p className="projectInfo">Director: {project.director}</p>
-                <p className="projectInfo">Year: {project.year}</p>
-                <p className="projectInfo">Role: {project.role}</p>
-              </div>
-            </div>
+          <div key={index} className="projectDetails">
+            <h3 className="projectTitle">
+              <a className="projectLink" href={project.link || "#"}>
+                {project.title}
+              </a>
+            </h3>
+            <p className="projectInfo">Director: {project.director}</p>
+            <p className="projectInfo">Year: {project.year}</p>
+            <p className="projectInfo">Role: {project.role}</p>
           </div>
         ))}
       </div>
